b4fa5d581e5beb54b1f7b1c56a4965692f9d9390
mohammed-0721 <mohammed.k08@infosys.com>
2024-03-27 13:52:01 +0530

    Test 2 added successfully in pushdemo

---
+++ Pushdemo/src/demo/Test2.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String Hungry = "Yes";
+		if(Hungry == "Hungry" || Hungry =="y" || Hungry =="Y")
+		{
+			System.out.println("eat food");
+		}
+		else
+		{
+			System.out.println("go to sleep");
+		}
+
+	}
+
+}
