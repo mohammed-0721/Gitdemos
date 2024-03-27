20a12aa45f11e1d1ce6ce6016d189011c5f94e87
mohammed-0721 <mohammed.k08@infosys.com>
2024-03-27 14:43:02 +0530

    Pushdemo2 pushed

---
+++ PushDemo2/src/demos/Firstpush.java
@@ -0,0 +1,18 @@
+package demos;
+
+public class Firstpush {
+
+	public static void main(String[] args) {
+		 int a = 10;
+		 if (a%2==0)
+		 {
+			 System.out.println("even no");
+		 }
+		 else
+		 {
+			 System.out.println("odd no");
+		 }
+
+	}
+
+}
