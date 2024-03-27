26f81709172d4074ecbbda4221695d2919086b7c
mohammed-0721 <mohammed.k08@infosys.com>
2024-03-27 11:12:28 +0530

    second one comitted successfully

---
+++ Pushdemo/src/demo/Ppushdemo.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Ppushdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hungry";
+		if(str == "Hungry")
+		{
+			System.out.println("eat food");
+		}
+		else
+		{
+			System.out.println("go to sleep");
+		}
+		
+
+	}
+
+}
